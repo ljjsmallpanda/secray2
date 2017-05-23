90bc78948b1c02c1bfbd9e63b87f20fdffdd348f
smallpanda <1766178745@qq.com>
2017-05-23 15:48:21 +0800

    构建学生pojo

---
+++ TestWeb-2/src/model/Student.java
@@ -0,0 +1,33 @@
+package model;
+/**
+ * TODO 学生
+ * @copyright {@link www.centforsoft.com}
+ * @author 
+ * @version 2017年5月23日 下午3:46:52
+ * @see model.Student
+ */
+public class Student {
+	private int stuid;
+	private String stuname;
+	private int sex;
+	public int getStuid() {
+		return stuid;
+	}
+	public void setStuid(int stuid) {
+		this.stuid = stuid;
+	}
+	public String getStuname() {
+		return stuname;
+	}
+	public void setStuname(String stuname) {
+		this.stuname = stuname;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	
+	
+}
